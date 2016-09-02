758e4162870596aaaf0a39be6a37654283a1c8ea
Raim <rai.interiano@gmail.com>
2016-09-02 11:00:48 -0600

    div operation for MIPS32 sim implemented

--- mips32_sim.cpp
+++ mips32_sim.cpp
@@ -654,6 +654,8 @@ bool MIPS32Sim::execInstruction(MInstruction *inst, MRtContext &ctx)
         case FN_SLLV: // sllv rd,rt,rs ; R Format
             break;
         case FN_DIV: // div rs,rt ; R Format
+            hi_lo = ((int32_t)*p0 / (int32_t)*p1);
+            hi_lo = (((uint64_t) ((int32_t)*p0 % (int32_t)*p1)) << 32) | hi_lo;
             break;
         case FN_SRLV: // srlv rd,rt,rs ; R Format
             break;
