83719155826b9cd9a1390f8209c243e84b0100dc
kenystev <kjestevez.0795@gmail.com>
2020-08-31 21:42:25 -0600

    Add custom lemon dependency and script to install all the i386 dependencies

---
+++ scripts/install-deps.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Install i386 dependencies
+sudo apt update
+sudo apt install gcc-multilib g++-multilib
+sudo apt install lib32readline6-dev libreadline-dev:i386 libedit-dev:i386
+
+# Compile and Install Custom version of Lemon
+cd scripts/lemon
+sudo chmod +x ./build.sh
+./build.sh
+sudo cp ./lemon /usr/bin/
+echo "Installed lemon into /usr/bin"
+ls /usr/bin | grep lemon
